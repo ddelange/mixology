6906aa77117a68609cbede539e87576d89c29be6
ddelange <14880945+ddelange@users.noreply.github.com>
2019-12-30 11:39:58 +0100

    Run black

--- mixology/version_solver.py
+++ mixology/version_solver.py
@@ -329,7 +329,11 @@ class VersionSolver:
             versions = self._source.versions_for(
                 term.package, term.constraint.constraint
             )
-            deps = self._source.dependencies_for(term.package, versions[0]) if versions else []
+            deps = (
+                self._source.dependencies_for(term.package, versions[0])
+                if versions
+                else []
+            )
             return len(versions), len(deps)
 
         if len(unsatisfied) == 1:
